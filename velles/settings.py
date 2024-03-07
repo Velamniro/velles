02230130ac7e3e749480b63e04f8af80ddda3ee4
Velamniro <nerzen4ik@gmail.com>
2024-03-07 10:27:54 +0500

    Migration 0002-0007 have been squashed

--- velles/settings.py
+++ velles/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o%*+je!&_g^4t324reltrbo^ftz@cif5_7f!4dam=_x1%^!v#8'
+SECRET_KEY = 'django-insecure-o%*+je!&_g^4t324reltrbo^ftz@ciy5_7f!4dam=_x1%^!v#8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
